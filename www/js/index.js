34b12258c9f9e964c718bef4a74b4437f7ffc613
icon-porker <14791286530@163.com>
2017-03-28 16:38:08 +0800

    11

---
+++ www/js/index.js
@@ -0,0 +1,16 @@
+﻿/**
+ * Created by 王泽良 on 2016/11/30.
+ */
+$(function(){
+
+   
+
+    $('#skin-blue').click(function(){
+        $('body').removeClass('skin-green').addClass('skin-blue')
+    });
+
+    $('.treeview-menu').find('a').click(function(){
+        $('.treeview-menu').find('a').removeClass('active-li');
+       $(this).addClass('active-li');
+    })
+});
